6d71219e163db1560cbbb8a2b3e4de280c6770d4
Asmaa933 <asmaatarek93@gmail.com>
2020-09-13 06:57:39 +0200

    Update widget_test.dart
    change widget name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:hodom_task/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(HodomApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
